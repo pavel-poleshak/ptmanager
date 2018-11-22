b3c3d9c0a4777184c58e9a49ec94362ffb15e65c
Pavel Poleshak <pavel.poleshak@gmail.com>
2018-11-22 23:47:55 +0200

    fixed destroying dependent objects of task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   belongs_to :status
   belongs_to :project
   has_many :task_items
-  has_many :users, through: :task_items
+  has_many :users, through: :task_items, dependent: :destroy
 end
