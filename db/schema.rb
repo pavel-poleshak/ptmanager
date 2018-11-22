ab1af94d08ef4f20d5e640c13670c86a37dcb17e
Pavel Poleshak <pavel.poleshak@gmail.com>
2018-11-22 21:32:56 +0200

    added TaskItem model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_190853) do
+ActiveRecord::Schema.define(version: 2018_11_22_192919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2018_11_22_190853) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "task_items", force: :cascade do |t|
+    t.bigint "task_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_task_items_on_task_id"
+    t.index ["user_id"], name: "index_task_items_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -56,6 +65,8 @@ ActiveRecord::Schema.define(version: 2018_11_22_190853) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "task_items", "tasks"
+  add_foreign_key "task_items", "users"
   add_foreign_key "tasks", "projects"
   add_foreign_key "tasks", "statuses"
 end
