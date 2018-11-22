89d94ac23ea2afa7c3db55cb34b393c40dd79ef8
Pavel Poleshak <pavel.poleshak@gmail.com>
2018-11-22 21:05:44 +0200

    added Status scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :statuses
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'projects#index'
   get 'projects/index', as: 'user_root'
