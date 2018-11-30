e664dda84c785e7c2e63f94412b87c6ab3e44415
Pavel Poleshak <pavel.poleshak@gmail.com>
2018-11-30 14:19:53 +0200

    presence validation for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
   belongs_to :project
   has_many :task_items
   has_many :users, through: :task_items, dependent: :destroy
-  validates :title, uniqueness: { scope: :project }
+  validates :title, presence: true, uniqueness: { scope: :project }
 end
